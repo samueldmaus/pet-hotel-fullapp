2537aab58c8a567d9d0ca94484c7c12720f8c7ac
Hannan Mir <hannanmir2@gmail.com>
2020-09-23 15:53:33 -0500

    file setup

---
+++ src/Components/owners/owners.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+
+class owners extends Component {
+  state = {
+  };
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(owners);
---
+++ src/Components/pets/pets.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+
+class pets extends Component {
+  state = {
+  };
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(pets);
